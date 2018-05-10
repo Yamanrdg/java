22316922f24825e981b01c0c2899bb6f3989cac2
Rabin Gaire <rabingaire20@gmail.com>
2018-05-10 19:40:55 +0545

    added Integer class

---
+++ Integer.java
@@ -0,0 +1,8 @@
+public class Integer {
+  public static void main(String[] args) {
+    int a; // variable declare 
+    a = -100; // variable assign
+
+    System.out.println(a); // I am printing value
+  }
+}
