e9dc143e0d8128ba36125ab3c878cb21b8d42f9e
Rabin Gaire <rabingaire20@gmail.com>
2018-05-14 22:05:02 +0545

    added MathCal class

---
+++ Concat.java
@@ -0,0 +1,10 @@
+public class Concat {
+  public static void main(String[] args) {
+    String a = "Age: ";
+    int b = 12;
+
+    String c = a + b;
+
+    System.out.println(c);
+  }
+}
---
+++ MathCal.java
@@ -0,0 +1,12 @@
+public class MathCal {
+  public static void main(String[] args) {
+    int a = 10;
+    double b = 20.0f;
+    System.out.println(a + b + 20);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b); // modulo returns reminder value after division
+    System.out.println(Math.pow(a, b));
+  }
+}
