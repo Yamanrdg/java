e18401166fb4e9a92707be251525014307e71246
Rabin Gaire <rabingaire20@gmail.com>
2018-05-17 21:53:52 +0545

    added Array file

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+  public static void main(String[] args) {
+    int a[] = { 12, 13 }; // Array defination
+
+    String b[] = new String[4];
+    b[0] = "Hello";
+
+    System.out.println(a[1]);
+    System.out.println(b[0]);
+  }
+}
