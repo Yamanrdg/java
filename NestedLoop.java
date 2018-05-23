ac55ea04cf80ad478af39ec972698b8e6b8855a8
Rabin Gaire <rabingaire20@gmail.com>
2018-05-23 22:31:53 +0545

    added NestedLoop.java file

---
+++ NestedLoop.java
@@ -0,0 +1,11 @@
+public class NestedLoop {
+  public static void main(String[] args) {
+    for (int a = 1; a <= 2; a++) {
+
+      for (int b = 1; b <= 2; b++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
