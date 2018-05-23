87d9fc39950c55cf0161f59cde030ff336116610
Rabin Gaire <rabingaire20@gmail.com>
2018-05-23 22:01:57 +0545

    added ArraysTwo.java

---
+++ ArraysTwo.java
@@ -0,0 +1,16 @@
+public class ArraysTwo {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    // length of arrays 3
+    // Index start with 0 - 2
+    int d[] = { 10, 20, 30 }; // Arrays
+
+    int e[] = new int[3];
+    e[0] = 10;
+    e[1] = 20;
+    e[2] = 30;
+  }
+}
