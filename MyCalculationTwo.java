e933fc46f1705df727802614342b92efeeb55a37
Rabin Gaire <rabingaire20@gmail.com>
2018-07-10 22:38:05 +0545

    added MyCalculationTwo.java

---
+++ MyCalculationTwo.java
@@ -0,0 +1,30 @@
+class Calculation { // Parent Class
+  public void add(int a, int b) {
+    int c = a + b;
+    System.out.print("Super Class Sum:");
+    System.out.println(c);
+  }
+
+  public void sub(int a, int b) {
+    int c = a - b;
+    System.out.print(c);
+  }
+}
+
+public class MyCalculationTwo extends Calculation { // Sub Class // Child Class
+  public void add(int a, int b) {
+    int c = a + b;
+    System.out.print("Sub Class Sum:");
+    System.out.println(c);
+  }
+
+  public void wrapperMethod() {
+    super.add(10, 20);
+  }
+
+  public static void main(String[] args) {
+    MyCalculationTwo calcTwo = new MyCalculationTwo();
+    calcTwo.add(200, 400);
+    calcTwo.wrapperMethod();
+  }
+}
